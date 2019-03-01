629bac6768eee5c39d1f7a754e2c09908e8e9853
ozdedzo <47147928+ozdedzo@users.noreply.github.com>
2019-03-01 22:45:24 +0300

    Updated mainWindow.py

--- mainWindow.py
+++ mainWindow.py
@@ -9,15 +9,22 @@ class MainWindow( QtWidgets.QMainWindow ):
 		self.centralWidget = QtWidgets.QWidget( self )
 		self.setCentralWidget( self.centralWidget )
 		# create a layout for central widget
-		self.layout = QtWidgets.QVBoxLayout( self.centralWidget )
+		self.layout = QtWidgets.QGridLayout( self.centralWidget )
+
+		self.historyBox = QtWidgets.QPlainTextEdit( self.centralWidget )
+		self.layout.addWidget( self.historyBox, 0, 0, -1, 1)
+		self.historyBox.setFixedSize( 150, 150 )
+
+		self.textBox = QtWidgets.QLineEdit( self.centralWidget )
+		self.layout.addWidget( self.textBox, 0, 1, 1, 1 )
+
+		self.datatextBox = QtWidgets.QLabel( self.centralWidget )
+		self.layout.addWidget( self.datatextBox, 1, 1, 1, 1 )
 
 		# parent of closeButton should be centralWidget, since the button should be in
 		self.closeButton = QtWidgets.QPushButton( self.centralWidget )
-		self.layout.addWidget( self.closeButton )
+		self.layout.addWidget( self.closeButton, 2, 1, 1, 1 )
 
-		# parent of printButton should be centralWidget, since the button should be in
-		self.printButton = QtWidgets.QPushButton( self.centralWidget )
-		self.layout.addWidget( self.printButton )
 
 		self.initSignalsAndSlots()
 
@@ -25,17 +32,32 @@ class MainWindow( QtWidgets.QMainWindow ):
 
 
 	def initSignalsAndSlots( self ):
+		self.historyBox.setReadOnly(True)
+		# self.showMessage implemented at below
+		self.textBox.editingFinished.connect( self.showAnswer )
 		# self.close is a built-in method that close the main window
 		self.closeButton.clicked.connect( self.close )
-		# self.showMessage implemented at below
-		self.printButton.clicked.connect( self.showMessage )
+
 
 
 	def _setName( self ):
 		# this is for setText of the buttons
+		self.textBox.setText( 'Enter a number' )
 		self.closeButton.setText( 'Close' )
-		self.printButton.setText( 'Show' )
 
 
-	def showMessage( self ):
-		QtWidgets.QMessageBox.warning(self,'Info Message',"Keep calm and keep calm")
+
+	def showAnswer( self ):
+		def factorial(n):
+			answer = 1
+			while n>1:
+				answer *= n
+				n -= 1
+			return answer
+		try:
+			number = self.textBox.text()
+			answer = str(factorial(int(number)))
+			self.datatextBox.setText( "Answer: \n"+number+"!="+answer )
+			self.historyBox.appendPlainText( number+"!="+answer )
+		except:
+			self.datatextBox.setText( 'Please type in an integer!' )
