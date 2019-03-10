38ad5e476a6d5c7eceabd558466672fbcd64ad1a
ozdedzo <47147928+ozdedzo@users.noreply.github.com>
2019-03-10 19:45:08 +0300

    Update mainWindow.py

--- mainWindow.py
+++ mainWindow.py
@@ -11,20 +11,22 @@ class MainWindow( QtWidgets.QMainWindow ):
 		# create a layout for central widget
 		self.layout = QtWidgets.QGridLayout( self.centralWidget )
 
-		self.historyBox = QtWidgets.QPlainTextEdit( self.centralWidget )
+		self.historyBox = QtWidgets.QListWidget( self.centralWidget )
 		self.layout.addWidget( self.historyBox, 0, 0, -1, 1)
 		self.historyBox.setFixedSize( 150, 150 )
 
 		self.textBox = QtWidgets.QLineEdit( self.centralWidget )
 		self.layout.addWidget( self.textBox, 0, 1, 1, 1 )
 
-		self.datatextBox = QtWidgets.QLabel( self.centralWidget )
+		self.datatextBox = QtWidgets.QLineEdit( self.centralWidget )
 		self.layout.addWidget( self.datatextBox, 1, 1, 1, 1 )
+		self.datatextBox.setFixedSize( 150, 89 )
 
 		# parent of closeButton should be centralWidget, since the button should be in
 		self.closeButton = QtWidgets.QPushButton( self.centralWidget )
 		self.layout.addWidget( self.closeButton, 2, 1, 1, 1 )
 
+		self.historyBox.itemClicked.connect( self.msgBox )
 
 		self.initSignalsAndSlots()
 
@@ -32,32 +34,43 @@ class MainWindow( QtWidgets.QMainWindow ):
 
 
 	def initSignalsAndSlots( self ):
-		self.historyBox.setReadOnly(True)
 		# self.showMessage implemented at below
-		self.textBox.editingFinished.connect( self.showAnswer )
+		self.textBox.returnPressed.connect( self.showAnswer )
 		# self.close is a built-in method that close the main window
 		self.closeButton.clicked.connect( self.close )
-
-
+		self.datatextBox.setReadOnly( True )
 
 	def _setName( self ):
 		# this is for setText of the buttons
-		self.textBox.setText( 'Enter a number' )
+		self.textBox.setPlaceholderText( 'Enter a number' )
 		self.closeButton.setText( 'Close' )
 
+	def msgBox( self ):
+		item = self.historyBox.currentItem()
+		num = item.text()
+		separate = num.find("!")
+		ans = int( num[:separate] )
+		recal = self.factorial( ans )
+		QtWidgets.QMessageBox.about( self, "Full Answer", num[:separate]+"!=" + str( recal ) )
 
+	def factorial( self, n ):
+		fac = 1
+		while n > 1:
+			fac *= n
+			n -= 1
+		return fac
 
 	def showAnswer( self ):
-		def factorial(n):
-			answer = 1
-			while n>1:
-				answer *= n
-				n -= 1
-			return answer
+		number = int( self.textBox.text() )
+		answer = str( self.factorial( number ) )
+		fullitem = str( number ) + "!=" + answer
 		try:
-			number = self.textBox.text()
-			answer = str(factorial(int(number)))
-			self.datatextBox.setText( "Answer: \n"+number+"!="+answer )
-			self.historyBox.appendPlainText( number+"!="+answer )
+			if len( answer ) <= 11:
+				self.datatextBox.setText( fullitem )
+				self.historyBox.addItem( fullitem )
+			else:
+				item = str( number ) + "!=" + answer[:12] + "..."
+				self.datatextBox.setText( item )
+				self.historyBox.addItem( item )
 		except:
 			self.datatextBox.setText( 'Please type in an integer!' )
